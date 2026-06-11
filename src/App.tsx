d2b38de963f01c2cc552f6f0cf9810f2b64b098f
Shinna <xjbdv1@163.com>
2026-06-11 20:47:00 +0800

    Update 1 file: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <div className='flex flex-col z-10 items-center justify-center h-screen pointer-events-none'>
           <div className='w-72 md:w-100 lg:w-111 bg-black/50 backdrop-blur-2xl justify-between'>
             <div className='flex flex-row justify-between w-full'>
-              <h1 className='text-5xl font-bold items-center justify-center text-[#E9FFAC] w-auto'>Megan L O’Donnell</h1>
+              <h1 className='text-5xl font-bold items-center justify-center text-[#E9FFAC] w-auto'>叶佩芳</h1>
               <h1 className='text-3xl font-bold items-center justify-center text-[#E9FFAC] w-auto'>+</h1>
             </div>
             <h1 className='text-xl font-bold items-center justify-center text-[#E9FFAC]/80'>要把生活当作小说看，又要把小说当作生活看。</h1>
